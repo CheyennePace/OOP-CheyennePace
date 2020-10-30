ab0e21953344dd06aa57a1ed38d809be7fd4d0d5
CheyennePace <CheyennePace@users.noreply.github.com>
2020-10-30 11:20:28 +0100

    .

---
+++ lesson 2/Assets/CircleSpawnerMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSpawnerMouse : MonoBehaviour
+{
+    Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);
+    }
+}
