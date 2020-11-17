dda04dda411de76e9cddc76b328e5465aeda6dce
CheyennePace <CheyennePace@users.noreply.github.com>
2020-11-17 09:39:55 +0100

    .

--- SuperCannon/Assets/Cannon.cs
+++ SuperCannon/Assets/Cannon.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class Cannon : MonoBehaviour
 {
-    public GameObject mybulletprefab;
     private Vector3 mousePos;
-    public Transform tipofcannon;
-
     // Start is called before the first frame update
     void Start()
     {
-        mybulletprefab = Resources.Load("prefabs/SmallBullet") as GameObject;
+        
     }
 
     // Update is called once per frame
@@ -23,13 +20,9 @@ public class Cannon : MonoBehaviour
         Quaternion newrotation = Quaternion.LookRotation(this.transform.position - mousePos, Vector3.forward);
         newrotation.x = 0f;
         newrotation.y = 0f;
-        //this.transform.rotation = newrotation;
+        //this.transform.rotation = newrotation;  //no slurp
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, newrotation, Time.deltaTime *4);
 
-        if(Input.GetButtonDown("Fire1"))
-        {
-            
-            Instantiate(mybulletprefab, this.gameObject.transform.GetChild(0).position, Quaternion.identity);
-        }
+
     }
 }
