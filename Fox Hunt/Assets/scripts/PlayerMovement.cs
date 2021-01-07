44ae1c8cbbd230ab7e3f507200b50ecef1678aac
CheyennePace <CheyennePace@users.noreply.github.com>
2021-01-07 22:16:49 +0100

    playerMovement

--- Fox Hunt/Assets/scripts/PlayerMovement.cs
+++ Fox Hunt/Assets/scripts/PlayerMovement.cs
@@ -5,40 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public CharacterController2D controller;
+public CharacterController2D controller;
 
-    float horizontalMove = 0f;
-    public float runSpeed = 40f;
+	public float runSpeed = 40f;
 
-    bool jump=false;
-    bool crouch = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+	
+	// Update is called once per frame
+	void Update () {
 
-    // Update is called once per frame
-    void Update()
-    {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        if(Input.GetButtonDown("Jump"))
-        {
-            jump = true;
-        }
-        if(Input.GetButtonDown("crouch"))
-        {
-            crouch= true;
-        }else if (Input.GetButtonUp("crouch"))
-        {
-            crouch = false;
-        }
-    }
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+		}
 
-    void FixedUpdate ()
-    {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
-        jump = false;
-    }
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
 }
