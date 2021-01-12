23bc7549cbd7ff8c3e2d9d33a5d6717d6b8592ad
CheyennePace <CheyennePace@users.noreply.github.com>
2021-01-12 21:07:22 +0100

    postioning + animation

--- Fox Hunt/Assets/scripts/PlayerMovement.cs
+++ Fox Hunt/Assets/scripts/PlayerMovement.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-public CharacterController2D controller;
+	public CharacterController2D controller;
+	public Animator animator;
 
 	public float runSpeed = 40f;
 
 	float horizontalMove = 0f;
 	bool jump = false;
 	bool crouch = false;
-	
+
 	// Update is called once per frame
 	void Update () {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
 		if (Input.GetButtonDown("Jump"))
 		{
 			jump = true;
+			animator.SetBool("IsJumping", true);
 		}
 
 		if (Input.GetButtonDown("Crouch"))
@@ -31,6 +34,16 @@ public CharacterController2D controller;
 			crouch = false;
 		}
 
+	}
+
+	public void OnLanding ()
+	{
+		animator.SetBool("IsJumping", false);
+	}
+
+	public void OnCrouching (bool isCrouching)
+	{
+		animator.SetBool("IsCrouching", isCrouching);
 	}
 
 	void FixedUpdate ()
