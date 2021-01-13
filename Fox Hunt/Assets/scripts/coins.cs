4b844a7a489b84ae60f50874260db6bde4d3272b
CheyennePace <CheyennePace@users.noreply.github.com>
2021-01-13 13:20:26 +0100

    coins and spikes ad prefabs

---
+++ Fox Hunt/Assets/scripts/coins.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coins : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+     void OnTriggerEnter2D(Collider2D col)
+    { 
+        if (col.name == "player")
+            FindObjectOfType<GameScore>().AddToScore();
+            Destroy(gameObject);
+    }
+}
